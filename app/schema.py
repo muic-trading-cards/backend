2670d902553e62574395bdc5fa7f439c1f3a6872
suphawit.won@student.mahidol.edu <you@example.com>
2022-12-04 10:33:48 +0700

    Change default value for permission_level for user to 2

--- app/schema.py
+++ app/schema.py
@@ -31,7 +31,7 @@ class User(Base, UserMixin):
     password = Column(String(100))
     first_name = Column(String(100))
     last_name = Column(String(100))
-    permission_level = Column(Integer, ForeignKey('permission_levels.id'), default=0)
+    permission_level = Column(Integer, ForeignKey('permission_levels.id'), default=2)
     created_at = Column(DateTime)
     listings = relationship('Listing', backref='owner')
     cards = relationship('Card', backref='owner')
